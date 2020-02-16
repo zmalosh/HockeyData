a084c3b505b759d2ece2affd64867f2c4077bdc7
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-02-16 18:44:47 -0500

    Add project files.

---
+++ HockeyData.Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HockeyData.Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ HockeyData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HockeyData
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
