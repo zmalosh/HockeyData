3c1aaa6f78c99e528839bc6b3e0e65686bef38e0
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-02-16 20:02:00 -0500

    implement SeasonsProcessor

---
+++ HockeyData.Processors/JsonUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace HockeyData.Processors
+{
+	public static class JsonUtility
+	{
+		private static readonly WebClient WebClient = CreateWebClient();
+
+		public static string GetRawJsonFromUrl(string url)
+		{
+			var rawJson = WebClient.DownloadString(url);
+			return rawJson;
+		}
+
+		private static WebClient CreateWebClient()
+		{
+			var client = new WebClient();
+			return client;
+		}
+	}
+}
---
+++ HockeyData.Processors/NhlCom/Processors/SeasonsProcessor.cs
@@ -0,0 +1,42 @@
+﻿using HockeyData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HockeyData.Processors.NhlCom.Processors
+{
+	public class SeasonsProcessor : IProcessor
+	{
+		public void Run(HockeyDataContext dbContext)
+		{
+			var url = Feeds.SeasonsFeed.GetFeedUrl();
+			var rawJson = JsonUtility.GetRawJsonFromUrl(url);
+			var feed = Feeds.SeasonsFeed.FromJson(rawJson);
+
+			int nhlLeagueId = dbContext.Leagues.Single(x => x.LeagueAbbr == "NHL").LeagueId;
+			var seasonsDict = dbContext.Seasons.ToDictionary(x => x.NhlSeasonKey, y => y);
+			var apiSeasons = feed.Seasons.OrderBy(x => x.SeasonId).ToList();
+
+			foreach (var apiSeason in apiSeasons)
+			{
+				if (!seasonsDict.ContainsKey(apiSeason.SeasonId))
+				{
+					var dbSeason = new Season
+					{
+						LeagueId = nhlLeagueId,
+						NhlSeasonKey = apiSeason.SeasonId,
+						HasConferences = apiSeason.ConferencesInUse,
+						HasDivisions = apiSeason.DivisionsInUse,
+						HasOlympics = apiSeason.OlympicsParticipation,
+						HasTies = apiSeason.TiesInUse,
+						HasWildCard = apiSeason.WildCardInUse
+					};
+					seasonsDict.Add(dbSeason.NhlSeasonKey, dbSeason);
+					dbContext.Seasons.Add(dbSeason);
+				}
+			}
+			dbContext.SaveChanges();
+		}
+	}
+}
