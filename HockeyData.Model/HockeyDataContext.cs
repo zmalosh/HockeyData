a7dae83f68959c6022a255c246557450aba33514
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-02-16 19:11:10 -0500

    add base dbContext

--- HockeyData.Model/HockeyDataContext.cs
+++ HockeyData.Model/HockeyDataContext.cs
@@ -1,9 +1,50 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace HockeyData.Model
 {
-	public class HockeyDataContext
+	public class HockeyDataContext : DbContext
 	{
+		private readonly IConfiguration config;
 
+		public HockeyDataContext(IConfiguration config) : base()
+		{
+			this.config = config;
+			this.ChangeTracker.Tracked += OnEntityTracked;
+			this.ChangeTracker.StateChanged += OnEntityStateChanged;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			//IConfigurationRoot configuration = new ConfigurationBuilder()
+			//	.SetBasePath(Directory.GetCurrentDirectory())
+			//	.AddJsonFile("appsettings.json")
+			//	.Build();
+			var connectionString = this.config["BasketballDataContextConnectionString"];
+			optionsBuilder.UseSqlServer(connectionString);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+		}
+
+		void OnEntityTracked(object sender, EntityTrackedEventArgs e)
+		{
+			if (!e.FromQuery && e.Entry.State == EntityState.Added && e.Entry.Entity is IEntity entity)
+			{
+				entity.DateCreatedUtc = DateTime.UtcNow;
+				entity.DateLastModifiedUtc = DateTime.UtcNow;
+			}
+		}
+
+		void OnEntityStateChanged(object sender, EntityStateChangedEventArgs e)
+		{
+			if (e.NewState == EntityState.Modified && e.Entry.Entity is IEntity entity)
+			{
+				entity.DateLastModifiedUtc = DateTime.UtcNow;
+			}
+		}
 	}
 }
